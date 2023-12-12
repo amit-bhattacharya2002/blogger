2df111bfce35d2a4f30782beb3652c7b0e551aa9
amit-bhattacharya2002 <77628382+amit-bhattacharya2002@users.noreply.github.com>
2023-12-12 14:43:46 -0800

    Blog centered - page.tsx

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -16,7 +16,7 @@ const page = async ({ params } :{
     })
 
   return (
-    <div>{post && <>
+    <div className="w-[70%] mx-auto">{post && <>
     <h1 className='text-2xl border shadow-[0.25rem_0.25rem_0px_0px_rgba(0,0,0,1)] sm:text-4xl mb-2 mt-2 px-2 py-2 sm:text-start text-center'> {post.title}</h1> 
     <p className='italic'>- {post.author.name}</p>
     <p className=' text-center sm:text-start'>{post.content}</p>
